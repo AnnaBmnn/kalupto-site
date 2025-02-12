2ea79032da916dcf215a7e6bb1ad53a4b1721c13
Anna <anna@akufen.ca>
2025-02-11 22:08:08 -0500

    setting

--- src/Experience/World/Videos.js
+++ src/Experience/World/Videos.js
@@ -14,7 +14,7 @@ export default class Videos
         this.time = this.experience.time
         this.audio = this.experience.world.audio
         this.videos = document.querySelectorAll('.js-video-texture')
-        this.numberSlide = 40
+        this.numberSlide = 20
         this.frame = 0
         this.frameAnim = 0
 
