08122035fb29e268d28f8d37547ec97b1bd1d512
Anna <anna@akufen.ca>
2025-04-01 18:59:25 -0400

    restart

--- src/Experience/Interface/Splash.js
+++ src/Experience/Interface/Splash.js
@@ -5,15 +5,20 @@ export default class Splash
         // Elements HTML
         this.screen = document.querySelector('.js-splash')
         this.button = document.querySelector('.js-remove-splash')
+        this.restart = document.querySelector('.js-splash-restart')
 
         // Binding
         this.onRemoveSplash = this.onRemoveSplash.bind(this)
 
         this.button.addEventListener('click', this.onRemoveSplash)
+        this.restart.addEventListener('click', ()=>{
+            location.reload();
+        })
 
     }
     onRemoveSplash()
     {
+        this.restart.classList.add('show')
         this.screen.classList.add('is-hidden')
     }
 }
